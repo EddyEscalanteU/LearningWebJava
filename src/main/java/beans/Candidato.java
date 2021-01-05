3462e849ea4e8b5063350b8abe2b4cb72493e02c
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2021-01-05 12:27:20 -0400

    create class Candidato

---
+++ src/main/java/beans/Candidato.java
@@ -0,0 +1,27 @@
+package beans;
+
+/*
+    Created by IntelliJ IDEA.
+    User: @EddyEscalanteU
+    Date: 05/01/2021
+    Time: 11:33 a. m.
+*/
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class Candidato {
+
+    private String nombre = "Introduce tu nombre";
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
